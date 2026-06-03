538fba062c06d9ea21d69e3dc87bf27a4420c6ff
Jishnu-ParthGarg <gargp6995@gmail.com>
2026-06-03 23:51:23 +0530

    Day-7 Programs

---
+++ Day-7/Q28.cpp
@@ -0,0 +1,15 @@
+//Write a program to Recursive reverse number
+#include <bits/stdc++.h>
+using namespace std;
+int reverseNum(int n,int rev=0){
+    if(n==0)
+        return rev;
+    return reverseNum(n/10,rev*10+n%10);
+}
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    cout<<"Reversed number = "<<reverseNum(n)<<endl;
+    return 0;
+}
