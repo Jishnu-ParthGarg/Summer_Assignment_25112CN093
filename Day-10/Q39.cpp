ca125ec7476c66387ca3ffbecd7583ef34b9f7f8
Jishnu-ParthGarg <gargp6995@gmail.com>
2026-06-05 23:51:50 +0530

    Day-10 Programs

---
+++ Day-10/Q39.cpp
@@ -0,0 +1,21 @@
+//Write a program to Print number pyramid.
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int i,j,k,n;
+    cout<<"Enter the Number of Rows: ";
+    cin>>n;
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n-i;j++){
+            cout<<" ";
+        }
+        for(k=1;k<=i;k++){
+            cout<<k;
+        }
+        for(k=i-1;k>=1;k--){
+            cout<<k;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
